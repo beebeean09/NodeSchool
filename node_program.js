fd72e087bf42ff2f2ff2635c5a4fa6c14894dd9d
beebeean09 <beebeean09@gmail.com>
2017-04-29 14:35:57 -0700

    complete 3 of learnyounode

---
+++ node_program.js
@@ -0,0 +1,26 @@
+// BABY STEPS
+// let array = process.argv.slice(2);
+// let sum = 0;
+//
+// array.forEach(el => {
+//   sum += parseInt(el);
+// });
+//
+// console.log(sum);
+
+// First SYNC
+// var fs = require('fs');
+//
+// var path = process.argv[2];
+// var buffer = fs.readFileSync(path);
+
+// console.log(process.argv[2]);
+// console.log(path);
+// console.log(buffer);
+// console.log(buffer.toString());
+
+// var contents = buffer.toString();
+// var length = contents.split('\n').length;
+// console.log(length - 1);
+
+// First ASYNC
